461bdb7bda04df3cabfdc0127d4fff9ca143f5bf
William L <w-lindvall@outlook.com>
2020-04-12 14:36:25 -0700

    use homepage content from cms

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import {graphql} from 'gatsby'
+import Img from 'gatsby-image'
 import get from 'lodash/get'
 import Helmet from 'react-helmet'
 import Layout from '../components/layout'
@@ -10,6 +11,7 @@ class RootIndex extends React.Component {
   render() {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
     const [author] = get(this, 'props.data.allContentfulAuthor.edges')
+    const {content, image} = get(this, 'props.data.contentfulPage')
 
     return (
       <Layout location={this.props.location}>
@@ -24,12 +26,16 @@ class RootIndex extends React.Component {
           <div className="wrapper">
             <h2 className="home-title">{author.node.name}</h2>
 
-            <p className="home-subtitle">
-              Hi, I'm Alexis, a recent McGill graduate with experience and
-              interest in the fields of environmental sustainability and social
-              impact. These are some beautiful wildflowers I saw while on a hike
-              in Manning Park, BC. Welcome to my site!
-            </p>
+            <p
+              className="home-subtitle"
+              dangerouslySetInnerHTML={{
+                __html: content.childMarkdownRemark.html,
+              }}
+            />
+
+            <div className="home-img">
+              <Img fixed={image.fixed} />
+            </div>
           </div>
 
           <Footer />
@@ -43,6 +49,21 @@ export default RootIndex
 
 export const pageQuery = graphql`
   query HomeQuery {
+    contentfulPage(contentful_id: {eq: "1sA3TZSZKiNpPwzJb27xiA"}) {
+      content {
+        childMarkdownRemark {
+          html
+        }
+      }
+      image {
+        fixed(width: 400) {
+          width
+          height
+          src
+          srcSet
+        }
+      }
+    }
     allContentfulAuthor(
       filter: {contentful_id: {eq: "3n9x5NFplvt5Hb9QVo6pN1"}}
     ) {
